6b768c4d92ed17e4e70ae9297fd8760478bcdd12
adriene <adriene@LAPTOP-SGVI283R>
2019-11-27 14:51:58 -0300

    Projeto

--- ControleFinanceiro/src/main/java/sc/senac/br/controlefinanceiro/dao/EmpresaDao.java
+++ ControleFinanceiro/src/main/java/sc/senac/br/controlefinanceiro/dao/EmpresaDao.java
@@ -11,27 +11,26 @@ import sc.senac.br.controlefinanceiro.model.Empresa;
 
 public class EmpresaDao extends BaseDao<Empresa> {
 
-	public List<EmpresasPorCategoriaDTO> buscaCategoriasPorEmpresa(){
+	public List<EmpresasPorCategoriaDTO> buscaCategoriasPorEmpresa() {
 		EntityManager manager = JPAUtil.getEntityManager();
-		
+
 		StringBuilder jpqlBuider = new StringBuilder();
 		jpqlBuider.append("select new sc.senac.br.controlefinanceiro.dto.EmpresasPorCategoriaDTO( ");
 		jpqlBuider.append(" cat.descricao, ");
 		jpqlBuider.append(" count(em) ");
-	    jpqlBuider.append(" ) ");
+		jpqlBuider.append(" ) ");
 		jpqlBuider.append("from Empresa em ");
 		jpqlBuider.append("right join em.categoria cat ");
 		jpqlBuider.append("group by cat.descricao ");
-		
-	TypedQuery<EmpresasPorCategoriaDTO> query = manager
-			.createQuery(jpqlBuider.toString(), EmpresasPorCategoriaDTO.class);
-	
-	List<EmpresasPorCategoriaDTO> resultado = query.getResultList();
-	
-	manager.close();
-	
+
+		TypedQuery<EmpresasPorCategoriaDTO> query = manager.createQuery(jpqlBuider.toString(),
+				EmpresasPorCategoriaDTO.class);
+
+		List<EmpresasPorCategoriaDTO> resultado = query.getResultList();
+
+		manager.close();
+
 		return resultado;
 	}
-	
 
 }
