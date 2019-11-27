11dff66a4d28018d593db413231d9cdce5a0c395
adriene <adriene@LAPTOP-SGVI283R>
2019-11-27 20:51:06 -0300

    Projeto Atualizado

---
+++ ControleFinanceiro/src/main/java/sc/senac/br/controlefinanceiro/model/Usuario.java
@@ -0,0 +1,76 @@
+package sc.senac.br.controlefinanceiro.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Usuario implements IBaseModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	private String nome;
+	private String email;
+	private String senha;
+
+	@Override
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	@Override
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
+}
